26fd0a65c3e9ce87205fc859efb9becfe535ca71
yz830620 <yz830620@gmail.com>
2021-06-03 01:49:55 +0800

    adding new posing and function

---
+++ posing/new_pos_project.py
@@ -0,0 +1,23 @@
+import cv2
+import time
+import PosModule as pm
+
+cap = cv2.VideoCapture('PosVideos/5.mp4')
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+detector = pm.poseDetector()
+pTime = 0
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    lmList = detector.findPosition(img)
+    if len(lmList) != 0:
+        print(lmList)
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (70,50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    cv2.imshow("Image", img)
+
+    cv2.waitKey(1)
